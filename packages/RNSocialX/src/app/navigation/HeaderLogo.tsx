952cc6009f2d6fcfdb0febdb51c73dbc4e004001
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-03 15:00:08 +0300

    Migrate HeartAnimation

--- packages/RNSocialX/src/app/navigation/HeaderLogo.tsx
+++ packages/RNSocialX/src/app/navigation/HeaderLogo.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Image, StyleSheet} from 'react-native';
+
 import {Images, Sizes} from '../../environment/theme';
 
 export const HeaderLogo = () => <Image source={Images.IntroWalkThrough1Logo} style={styles.logo} />;
