c80590166c2adb925614fe87488ed088a3bd7028
ionutmovila <ionut.movila@sensidev.com>
2018-09-03 18:13:52 +0300

    Moved HeaderLogo ti components/navigation + exports from components/index

--- packages/RNSocialX/src/components/index.ts
+++ packages/RNSocialX/src/components/index.ts
@@ -1,3 +1,5 @@
 export {Foo} from './Foo';
 export {SXTextInput, TKeyboardKeys, TRKeyboardKeys} from './inputs/SXTextInput';
 export {CommentTextInput} from './inputs/CommentTextInput';
+export {HeaderLogo} from './navigation/HeaderLogo';
+export {HeartAnimation} from './displayers/HeartAnimation';
