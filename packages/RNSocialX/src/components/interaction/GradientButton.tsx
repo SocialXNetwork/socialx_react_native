320baf0ada5af40fbfeb4e3c54891054750c5be8
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-04 11:54:36 +0300

    Migrated GradientButton

---
+++ packages/RNSocialX/src/components/interaction/GradientButton.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import {Colors, Sizes} from '../../environment/theme';
+import {IPrimaryButtonProps, PrimaryButton} from './PrimaryButton';
+
+interface IGradientButtonProps extends IPrimaryButtonProps {
+	colorStart: string;
+	colorEnd: string;
+}
+
+export const GradientButton: React.SFC<IGradientButtonProps> = (props) => {
+	const {colorStart, colorEnd, disabled, loading, size, ...buttonProps} = props;
+
+	const buttonDisabled = disabled || loading;
+
+	return (
+		<LinearGradient
+			start={{x: 0, y: 0.5}}
+			end={{x: 1, y: 0.5}}
+			colors={[colorStart, colorEnd]}
+			style={[style.container, buttonDisabled ? style.disabledButton : {}]}
+		>
+			<PrimaryButton {...buttonProps} containerStyle={style.innerButtonContainer} />
+		</LinearGradient>
+	);
+};
+
+const style = StyleSheet.create({
+	container: {
+		borderRadius: Sizes.smartVerticalScale(50) / 2,
+	},
+	disabledButton: {
+		opacity: 0.5,
+	},
+	innerButtonContainer: {
+		backgroundColor: Colors.transparent,
+	},
+});
