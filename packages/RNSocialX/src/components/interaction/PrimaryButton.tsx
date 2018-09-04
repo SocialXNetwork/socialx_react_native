28d279647402cbf01788ed9ccd23d98567dabd0b
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-04 11:27:32 +0300

    Migrated Button as PrimaryButton

---
+++ packages/RNSocialX/src/components/interaction/PrimaryButton.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import {ActivityIndicator, StyleProp, Text, TouchableOpacity, View, ViewStyle} from 'react-native';
+
+import {defaultColor, styles} from './PrimaryButton.style';
+
+export enum ButtonSizes {
+	Small = 'Small',
+	Normal = 'Normal',
+	Large = 'Large',
+}
+
+export interface IPrimaryButtonProps {
+	label: string;
+	width?: number;
+	disabled?: boolean;
+	onPress: () => void;
+	size?: ButtonSizes;
+	autoWidth?: boolean;
+	borderColor?: string;
+	textColor?: string;
+	loading?: boolean;
+	containerStyle?: StyleProp<ViewStyle>;
+}
+
+export const PrimaryButton: React.SFC<IPrimaryButtonProps> = ({
+	borderColor,
+	size,
+	containerStyle,
+	label,
+	loading,
+	onPress,
+	disabled,
+	width,
+	autoWidth,
+	textColor,
+}) => {
+	const buttonDisabled = disabled || loading;
+
+	const containerStyles = [
+		styles.container,
+		{borderColor},
+		containerStyle ? containerStyle : {},
+		styles['container' + size],
+		buttonDisabled ? styles.disabledButton : {},
+	];
+
+	let containerWidth: StyleProp<ViewStyle> = {width: '100%'};
+	if (width) {
+		containerWidth = {width};
+	} else if (autoWidth) {
+		containerWidth = {};
+	}
+
+	const textStyles = [styles.text, {color: textColor ? textColor : defaultColor}, size ? styles['text' + size] : {}];
+
+	return (
+		<TouchableOpacity disabled={buttonDisabled} onPress={onPress} style={containerWidth}>
+			<View style={containerStyles}>
+				<Text style={textStyles}>{label}</Text>
+				{loading && <ActivityIndicator size={'small'} color={defaultColor} style={styles.loadingIndicator} />}
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+PrimaryButton.defaultProps = {
+	width: 0,
+	disabled: false,
+	size: ButtonSizes.Normal,
+	autoWidth: false,
+	borderColor: defaultColor,
+	textColor: defaultColor,
+	loading: false,
+	containerStyle: {},
+};
