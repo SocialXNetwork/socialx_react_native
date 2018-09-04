b20033c43acea57da8e7c9b115a8f1e97c008f2b
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-04 11:54:36 +0300

    Migrated GradientButton

--- packages/RNSocialX/src/components/index.ts
+++ packages/RNSocialX/src/components/index.ts
@@ -5,8 +5,10 @@ export {HeartAnimation} from './displayers/HeartAnimation';
 export {CommentTextInput} from './inputs/CommentTextInput';
 export {SXTextInput, TKeyboardKeys, TRKeyboardKeys} from './inputs/SXTextInput';
 
+export {GradientButton} from './interaction/GradientButton';
 export {IconButton} from './interaction/IconButton';
 export {LikeAnimatingButton} from './interaction/LikeAnimatingButton';
+export {PrimaryButton, IPrimaryButtonProps} from './interaction/PrimaryButton';
 export {ToggleIconButton} from './interaction/ToggleIconButton';
 export {TouchableWithDoublePress} from './interaction/TouchableWithDoublePress';
 
