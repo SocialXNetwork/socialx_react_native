8f9462f3efbc269ea96da3c947db9fd915aaa1a5
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-05 16:19:55 +0300

    Migrated TabBarBottom as NavigationTabBar

---
+++ packages/RNSocialX/src/components/displayers/NavigationTabBar/NavigationTabBar.style.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {Colors, Sizes} from '../../../environment/theme';
+
+const styles: any = {
+	container: {
+		flexDirection: 'row',
+		width: '100%',
+		backgroundColor: Colors.tabBarBottomBg,
+	},
+};
+
+export default StyleSheet.create(styles);
