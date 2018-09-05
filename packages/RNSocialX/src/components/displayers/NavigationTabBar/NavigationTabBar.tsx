2ffef7ff3b9ffe9cb31f7f94746747a4f1409966
ionutmovila <ionut.movila@sensidev.com>
2018-09-05 16:46:56 +0300

    Small fixes after review NavigationTabBar

--- packages/RNSocialX/src/components/displayers/NavigationTabBar/NavigationTabBar.tsx
+++ packages/RNSocialX/src/components/displayers/NavigationTabBar/NavigationTabBar.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import {SafeAreaView} from 'react-native';
 import {LayoutEvent, NavigationScreenProp} from 'react-navigation';
 
-import {connect} from 'react-redux';
 import {ITranslatedProps} from '../../../types';
 import {
 	getCameraMediaObjectMultiple,
@@ -17,42 +16,19 @@ import styles from './NavigationTabBar.style';
 
 const INITIAL_SCREEN = 'UserFeedTab';
 
-////////////////////////////////////////////////////////////////
-// These types will be moved once Redux is up and running
-////////////////////////////////////////////////////////////////
-export interface INavigationState {
-	tabBarHeight: number;
-	newNotifications: number;
-}
-
-////////////////////////////////////////////////////////////////
-// This action type will be moved once Redux is up and running
-////////////////////////////////////////////////////////////////
-const UPDATE_TAB_BAR_HEIGHT = 'update_tab_bar_height';
-
-////////////////////////////////////////////////////////////////
-// This action will be moved once Redux is up and running
-////////////////////////////////////////////////////////////////
-const updateTabBarHeight = (height: number) => {
-	return {
-		type: UPDATE_TAB_BAR_HEIGHT,
-		payload: height,
-	};
-};
-
 interface ITabBarBottomState {
 	selectedTab: string;
 	changeInProgress: boolean;
 }
 
-interface ITabBarBottomProps extends INavigationState, ITranslatedProps {
+interface ITabBarBottomProps extends ITranslatedProps {
 	navigation: NavigationScreenProp<any>;
 	updateTabBarHeight: (value: number) => void;
 	tabHeight: number;
-	notifications: any;
+	notifications: any; // TODO: this can be just a number?
 }
 
-class NavigationTabBarComponent extends React.Component<ITabBarBottomProps, ITabBarBottomState> {
+export class NavigationTabBar extends React.Component<ITabBarBottomProps, ITabBarBottomState> {
 	public state = {
 		selectedTab: INITIAL_SCREEN,
 		changeInProgress: false,
@@ -115,7 +91,6 @@ class NavigationTabBarComponent extends React.Component<ITabBarBottomProps, ITab
 					selectedMediaObjects = await getCameraMediaObjectMultiple();
 				}
 
-				console.log('selectedMediaObjects', selectedMediaObjects);
 				if (selectedMediaObjects.length > 0) {
 					const optimizedMediaObjects = await Promise.all(
 						selectedMediaObjects.map(async (mediaObject: PickerImage) => getOptimizedMediaObject(mediaObject)),
@@ -126,12 +101,3 @@ class NavigationTabBarComponent extends React.Component<ITabBarBottomProps, ITab
 		);
 	};
 }
-
-const mapStateToProps = (state: any) => ({
-	tabHeight: state.navigation.tabBarHeight,
-});
-
-export const NavigationTabBar = connect(
-	mapStateToProps,
-	{updateTabBarHeight},
-)(NavigationTabBarComponent);
