660326c9c6e1b5bc8472442471c42e26e824f6c6
ionutmovila <ionut.movila@sensidev.com>
2018-09-05 15:37:13 +0300

    Migrated SettingCheckbox + removed metrics due to single prop not used in old repo.

--- packages/RNSocialX/src/environment/theme/Metrics.ts
+++
@@ -1,6 +0,0 @@
-import {Platform} from 'react-native';
-import {IosType} from '../consts';
-
-export const Metrics = {
-	navBarHeight: Platform.OS === IosType ? 54 : 47, // not very reliable should be done else!
-};
