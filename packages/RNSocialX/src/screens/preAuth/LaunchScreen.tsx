3f56ec5eab89775878b4170cb703c46340c8ec9c
ionutmovila <ionut.movila@sensidev.com>
2018-09-10 13:02:44 +0300

    Migrated LaunchScreen

---
+++ packages/RNSocialX/src/screens/preAuth/LaunchScreen.tsx
@@ -0,0 +1,73 @@
+/**
+ * TODO list:
+ * 1. currentUser
+ * 2. applicationInMaintenanceMode
+ * 3. resetNavigationToRoute, old repo. Internals/backend/actions/navigation.ts
+ * 3.1 we can do this at the top level, without navigation
+ */
+
+import * as React from 'react';
+import {LaunchScreenView} from './LaunchScreen.view';
+
+import SplashScreen from 'react-native-smart-splash-screen';
+import {NavigationScreenProp} from 'react-navigation';
+import {ITranslatedProps} from '../../types';
+
+export interface ILaunchScreenProps extends ITranslatedProps {
+	navigation: NavigationScreenProp<any>;
+	currentUser: any;
+	applicationInMaintenanceMode: boolean;
+	resetNavigationToRoute: (screenName: string, navigation: NavigationScreenProp<any>) => void;
+}
+
+export default class LaunchScreen extends React.Component<ILaunchScreenProps, any> {
+	private static navigationOptions = {
+		header: null,
+	};
+
+	public componentDidMount() {
+		// TODO: @Serkan, refactor and don't use navigation, put at the top level
+
+		const {currentUser, applicationInMaintenanceMode, resetNavigationToRoute} = this.props;
+		if (currentUser) {
+			if (__DEV__) {
+				resetNavigationToRoute('MainScreen', this.props.navigation);
+			} else {
+				if (applicationInMaintenanceMode) {
+					resetNavigationToRoute('Maintenance', this.props.navigation);
+				} else {
+					resetNavigationToRoute('MainScreen', this.props.navigation);
+				}
+			}
+		}
+
+		this.closeSplashScreen();
+	}
+
+	public render() {
+		const {getText} = this.props;
+		return (
+			<LaunchScreenView
+				getText={getText}
+				navigateToLoginScreen={this.navigateToLoginScreen}
+				navigateToSignUpScreen={this.navigateToSignUpScreen}
+			/>
+		);
+	}
+
+	private navigateToLoginScreen = () => {
+		this.props.navigation.navigate('LoginScreen');
+	};
+
+	private navigateToSignUpScreen = () => {
+		this.props.navigation.navigate('SignUpScreen');
+	};
+
+	private closeSplashScreen = () => {
+		SplashScreen.close({
+			animationType: SplashScreen.animationType.fade,
+			duration: 1000,
+			delay: 100,
+		});
+	};
+}
---
+++ packages/RNSocialX/src/screens/preAuth/LaunchScreen.view.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {Image, Text, View} from 'react-native';
+
+import {GradientButton, PrimaryButton, TextGradient} from '../../components';
+import {ITranslatedProps} from '../../types';
+import style, {customStylesProps} from './LaunchScreen.style';
+
+interface ILaunchScreenViewProps extends ITranslatedProps {
+	navigateToSignUpScreen: () => void;
+	navigateToLoginScreen: () => void;
+}
+
+export const LaunchScreenView: React.SFC<ILaunchScreenViewProps> = ({
+	getText,
+	navigateToSignUpScreen,
+	navigateToLoginScreen,
+}) => (
+	<View style={style.container}>
+		<Image source={customStylesProps.launchScreenBackground} style={style.background} resizeMode={'cover'} />
+		<View style={style.topPaddingContainer}>
+			<TextGradient
+				text={getText('app.name')}
+				colors={[customStylesProps.gradientTextStartColor, customStylesProps.gradientTextEndColor]}
+				style={style.socialxGradient}
+			/>
+			<Text style={style.description}>{getText('launch.description')}</Text>
+		</View>
+		<TextGradient
+			text={getText('launch.get.rewarded')}
+			colors={[customStylesProps.gradientTextEndColor, customStylesProps.gradientTextEndColor]}
+			style={style.getRewardedGradient}
+		/>
+		<View style={style.bottomPaddingContainer}>
+			// @ts-ignore
+			<GradientButton
+				colorStart={customStylesProps.gradientTextStartColor}
+				colorEnd={customStylesProps.gradientTextEndColor}
+				label={getText('launch.login')}
+				borderColor={customStylesProps.transparentBorderColor}
+				onPress={navigateToLoginScreen}
+			/>
+			<View style={style.signUpTopPadding}>
+				// @ts-ignore
+				<PrimaryButton
+					label={getText('launch.signUp')}
+					borderColor={customStylesProps.transparentBorderColor}
+					onPress={navigateToSignUpScreen}
+				/>
+			</View>
+		</View>
+	</View>
+);
