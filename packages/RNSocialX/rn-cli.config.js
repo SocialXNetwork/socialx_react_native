26175da2a00686230fb34041a0cf404bc12303e9
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-09-12 14:04:35 +0300

    change paths to explicit mapping to help mitigate build quirks

--- packages/RNSocialX/rn-cli.config.js
+++ packages/RNSocialX/rn-cli.config.js
@@ -1,27 +1,27 @@
 "use strict";
 
 const path = require("path");
-const cwd = path.resolve(__dirname);
-
-function getRoots() {
-  return [
-    cwd,
-    path.resolve(cwd, "../..")
-  ];
-}
 
 const config = {
   getProjectRoots() {
-    return getRoots();
+    return [
+      path.resolve(__dirname),
+      path.resolve(__dirname, "../commons"),
+      path.resolve(__dirname, "../api-storage"),
+      path.resolve(__dirname, "../api-blockchain"),
+      path.resolve(__dirname, "../api-data")
+    ];
   },
   getAssetRoots() {
-    return getRoots();
+    return [
+      path.resolve(__dirname)
+    ];
   },
   extraNodeModules: {
-    crypto: path.resolve(cwd, "./node_modules/react-native-crypto"),
-    stream: path.resolve(cwd, "./node_modules/react-native-stream"),
-    randombytes: path.resolve(cwd, "./node_modules/react-native-randombytes"),
-    vm: path.resolve(cwd, "./node_modules/vm-browserify")
+    crypto: path.resolve(__dirname, "./node_modules/react-native-crypto"),
+    stream: path.resolve(__dirname, "./node_modules/react-native-stream"),
+    randombytes: path.resolve(__dirname, "./node_modules/react-native-randombytes"),
+    vm: path.resolve(__dirname, "./node_modules/vm-browserify")
   },
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
