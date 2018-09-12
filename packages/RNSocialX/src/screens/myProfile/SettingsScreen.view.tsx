6028a08a1c87462cd37d1e63f4ff6e9d43de20bd
ionutmovila <ionut.movila@sensidev.com>
2018-09-12 12:57:43 +0300

    Updated Settings screen after review. Clean Xcode project, remove duplicate vector fonts

--- packages/RNSocialX/src/screens/myProfile/SettingsScreen.view.tsx
+++ packages/RNSocialX/src/screens/myProfile/SettingsScreen.view.tsx
@@ -27,182 +27,177 @@ export interface SettingsData {
 	lastName: string;
 	email: string;
 	miningEnabled: boolean;
-	avatarImage: ImageURISource;
+	avatarURL: string | null;
 	username: string;
 }
 
-interface ISettingsFormProps extends SettingsData, ITranslatedProps {
+interface ISettingsScreenViewProps extends SettingsData, ITranslatedProps, IWithLoaderProps {
 	onSaveChanges: (values: SettingsData) => void;
 }
 
-interface ISettingsScreenViewProps extends ISettingsFormProps, IWithLoaderProps {}
-
-const SettingsForm: React.SFC<ISettingsFormProps> = ({
+export const SettingsScreenView: React.SFC<ISettingsScreenViewProps> = ({
 	aboutText,
 	firstName,
 	lastName,
 	email,
-	avatarImage,
+	avatarURL,
 	username,
 	miningEnabled,
 	getText,
 	onSaveChanges,
+	isLoading,
 }) => (
-	<Formik
-		initialValues={{
-			aboutText,
-			firstName,
-			lastName,
-			email,
-			avatarImage,
-			username,
-			miningEnabled,
-		}}
-		validate={({firstName: firstNameValue, lastName: lastNameValue, email: emailValue}: SettingsData) => {
-			const errors: FormikErrors<SettingsData> = {};
-			if (!emailValue) {
-				errors.email = getText('settings.screen.email.required');
-			} else if (!EMAIL_SCHEMA.isValidSync(email)) {
-				errors.email = getText('settings.screen.email.invalid');
-			}
-			if (!firstNameValue) {
-				errors.firstName = getText('settings.screen.first.name.required');
-			}
-			if (!lastNameValue) {
-				errors.lastName = getText('settings.screen.last.name.required');
-			}
-			return errors;
-		}}
-		onSubmit={async (values: SettingsData, {resetForm, setErrors}: FormikActions<SettingsData>) => {
-			await onSaveChanges(values);
-			resetForm(values);
-		}}
-		render={({
-			values: {
-				aboutText: aboutTextValue,
-				firstName: firstNameValue,
-				lastName: lastNameValue,
-				email: emailValue,
-				avatarImage: avatarImageValue,
-				username: usernameValue,
-				miningEnabled: miningEnabledValue,
-			},
-			errors,
-			handleBlur,
-			handleSubmit,
-			isValid,
-			setFieldValue,
-		}: FormikProps<SettingsData>) => (
-			<View style={{flex: 1}}>
-				<KeyboardAwareScrollView
-					style={style.keyboardView}
-					contentContainerStyle={style.container}
-					alwaysBounceVertical={false}
-					enableOnAndroid={true}
-					keyboardShouldPersistTaps={'handled'}
-				>
-					<View style={style.pickerContainer}>
-						<AvatarPicker
-							avatarImage={avatarImageValue}
-							afterImagePick={(localPhotoPath: string) => setFieldValue('avatarImage', {uri: localPhotoPath}, false)}
-							avatarSize={customStyleProps.avatarPickerSize}
-							getText={getText}
+	<WithInlineLoader isLoading={isLoading}>
+		<Formik
+			initialValues={{
+				aboutText,
+				firstName,
+				lastName,
+				email,
+				avatarURL,
+				username,
+				miningEnabled,
+			}}
+			validate={({firstName: firstNameValue, lastName: lastNameValue, email: emailValue}: SettingsData) => {
+				const errors: FormikErrors<SettingsData> = {};
+				if (!emailValue) {
+					errors.email = getText('settings.screen.email.required');
+				} else if (!EMAIL_SCHEMA.isValidSync(email)) {
+					errors.email = getText('settings.screen.email.invalid');
+				}
+				if (!firstNameValue) {
+					errors.firstName = getText('settings.screen.first.name.required');
+				}
+				if (!lastNameValue) {
+					errors.lastName = getText('settings.screen.last.name.required');
+				}
+				return errors;
+			}}
+			onSubmit={async (values: SettingsData, {resetForm, setErrors}: FormikActions<SettingsData>) => {
+				await onSaveChanges(values);
+				resetForm(values);
+			}}
+			render={({
+				values: {
+					aboutText: aboutTextValue,
+					firstName: firstNameValue,
+					lastName: lastNameValue,
+					email: emailValue,
+					avatarURL: avatarURLValue,
+					username: usernameValue,
+					miningEnabled: miningEnabledValue,
+				},
+				errors,
+				handleBlur,
+				handleSubmit,
+				isValid,
+				setFieldValue,
+			}: FormikProps<SettingsData>) => (
+				<View style={{flex: 1}}>
+					<KeyboardAwareScrollView
+						style={style.keyboardView}
+						contentContainerStyle={style.container}
+						alwaysBounceVertical={false}
+						enableOnAndroid={true}
+						keyboardShouldPersistTaps={'handled'}
+					>
+						<View style={style.pickerContainer}>
+							<AvatarPicker
+								avatarImage={avatarURLValue !== null ? {uri: avatarURLValue} : customStyleProps.avatarPlaceholderImg}
+								afterImagePick={(localPhotoPath: string) => setFieldValue('avatarURL', localPhotoPath, false)}
+								avatarSize={customStyleProps.avatarPickerSize}
+								getText={getText}
+							/>
+						</View>
+						<AvatarName
+							fullName={firstNameValue + ' ' + lastNameValue}
+							username={usernameValue}
+							fullNameColor={customStyleProps.avatarFullNameColor}
+							userNameColor={customStyleProps.avatarUserNameColor}
 						/>
-					</View>
-					<AvatarName
-						fullName={firstNameValue + ' ' + lastNameValue}
-						username={usernameValue}
-						fullNameColor={customStyleProps.avatarFullNameColor}
-						userNameColor={customStyleProps.avatarUserNameColor}
-					/>
-					<View style={style.aboutContainer}>
-						<PrimaryTextInput
-							autoCapitalize={'sentences'}
-							autoCorrect={true}
-							value={aboutTextValue}
-							placeholder={getText('settings.screen.about.text.placeholder')}
-							borderColor={customStyleProps.aboutTextBorderColor}
-							multiline={true}
-							onChangeText={(value: string) => setFieldValue('aboutText', value)}
-							focusUpdateHandler={(value) => !value && handleBlur('aboutText')}
-						/>
-					</View>
-					<Text style={style.personalDetails}>{getText('settings.screen.personal.details')}</Text>
-					<View style={[style.textInputContainer, style.textInputContainerFirst]}>
-						<PrimaryTextInput
-							autoCapitalize={'words'}
-							autoCorrect={true}
-							value={firstNameValue}
-							iconColor={customStyleProps.userDataInputIconColor}
-							placeholder={getText('settings.screen.first.name.placeholder')}
-							placeholderColor={customStyleProps.userDataInputPlaceholderColor}
-							size={InputSizes.Small}
-							borderColor={customStyleProps.userDataInputBorderColor}
-							blurOnSubmit={true}
-							returnKeyType={TRKeyboardKeys.done}
-							onChangeText={(value: string) => setFieldValue('firstName', value)}
-							focusUpdateHandler={(value) => !value && handleBlur('firstName')}
-						/>
-						{errors.firstName && <Text style={style.errorText}>{errors.firstName}</Text>}
-					</View>
-					<View style={[style.textInputContainer]}>
-						<PrimaryTextInput
-							autoCapitalize={'words'}
-							autoCorrect={true}
-							value={lastNameValue}
-							iconColor={customStyleProps.userDataInputIconColor}
-							placeholder={getText('settings.screen.last.name.placeholder')}
-							placeholderColor={customStyleProps.userDataInputPlaceholderColor}
-							borderColor={customStyleProps.userDataInputBorderColor}
-							size={InputSizes.Small}
-							blurOnSubmit={true}
-							returnKeyType={TRKeyboardKeys.done}
-							onChangeText={(value: string) => setFieldValue('lastName', value)}
-							focusUpdateHandler={(value) => !value && handleBlur('lastName')}
-						/>
-						{errors.lastName && <Text style={style.errorText}>{errors.lastName}</Text>}
-					</View>
-					<View style={[style.textInputContainer]}>
-						<PrimaryTextInput
-							autoCapitalize={'words'}
-							value={emailValue}
-							iconColor={customStyleProps.userDataInputIconColor}
-							placeholder={getText('settings.screen.email.placeholder')}
-							placeholderColor={customStyleProps.userDataInputPlaceholderColor}
-							borderColor={customStyleProps.userDataInputBorderColor}
-							size={InputSizes.Small}
-							keyboardType={TKeyboardKeys.emailAddress}
-							blurOnSubmit={true}
-							returnKeyType={TRKeyboardKeys.done}
-							onChangeText={(value: string) => setFieldValue('email', value)}
-							focusUpdateHandler={(value) => !value && handleBlur('email')}
-						/>
-						{errors.email && <Text style={style.errorText}>{errors.email}</Text>}
-					</View>
-					<View style={style.miningContainer}>
-						<SettingCheckbox
-							title={getText('settings.screen.mining.title')}
-							description={getText('settings.screen.mining.description')}
-							value={miningEnabledValue}
-							onValueUpdated={() => setFieldValue('miningEnabled', !miningEnabledValue, false)}
-						/>
-					</View>
-				</KeyboardAwareScrollView>
-				{isValid && (
-					<View style={style.bottomContainer}>
-						<TouchableOpacity style={style.saveButton} onPress={handleSubmit}>
-							<Text style={style.saveButtonText}>{getText('settings.screen.save.button')}</Text>
-							<Icon name={'check'} style={style.checkIcon} />
-						</TouchableOpacity>
-					</View>
-				)}
-			</View>
-		)}
-	/>
-);
-
-export const SettingsScreenView: React.SFC<ISettingsScreenViewProps> = (props) => (
-	<WithInlineLoader isLoading={props.isLoading}>
-		<SettingsForm {...props} />
+						<View style={style.aboutContainer}>
+							<PrimaryTextInput
+								autoCapitalize={'sentences'}
+								autoCorrect={true}
+								value={aboutTextValue}
+								placeholder={getText('settings.screen.about.text.placeholder')}
+								borderColor={customStyleProps.aboutTextBorderColor}
+								multiline={true}
+								onChangeText={(value: string) => setFieldValue('aboutText', value)}
+								focusUpdateHandler={(value) => !value && handleBlur('aboutText')}
+							/>
+						</View>
+						<Text style={style.personalDetails}>{getText('settings.screen.personal.details')}</Text>
+						<View style={[style.textInputContainer, style.textInputContainerFirst]}>
+							<PrimaryTextInput
+								autoCapitalize={'words'}
+								autoCorrect={true}
+								value={firstNameValue}
+								iconColor={customStyleProps.userDataInputIconColor}
+								placeholder={getText('settings.screen.first.name.placeholder')}
+								placeholderColor={customStyleProps.userDataInputPlaceholderColor}
+								size={InputSizes.Small}
+								borderColor={customStyleProps.userDataInputBorderColor}
+								blurOnSubmit={true}
+								returnKeyType={TRKeyboardKeys.done}
+								onChangeText={(value: string) => setFieldValue('firstName', value)}
+								focusUpdateHandler={(value) => !value && handleBlur('firstName')}
+							/>
+							{errors.firstName && <Text style={style.errorText}>{errors.firstName}</Text>}
+						</View>
+						<View style={[style.textInputContainer]}>
+							<PrimaryTextInput
+								autoCapitalize={'words'}
+								autoCorrect={true}
+								value={lastNameValue}
+								iconColor={customStyleProps.userDataInputIconColor}
+								placeholder={getText('settings.screen.last.name.placeholder')}
+								placeholderColor={customStyleProps.userDataInputPlaceholderColor}
+								borderColor={customStyleProps.userDataInputBorderColor}
+								size={InputSizes.Small}
+								blurOnSubmit={true}
+								returnKeyType={TRKeyboardKeys.done}
+								onChangeText={(value: string) => setFieldValue('lastName', value)}
+								focusUpdateHandler={(value) => !value && handleBlur('lastName')}
+							/>
+							{errors.lastName && <Text style={style.errorText}>{errors.lastName}</Text>}
+						</View>
+						<View style={[style.textInputContainer]}>
+							<PrimaryTextInput
+								autoCapitalize={'words'}
+								value={emailValue}
+								iconColor={customStyleProps.userDataInputIconColor}
+								placeholder={getText('settings.screen.email.placeholder')}
+								placeholderColor={customStyleProps.userDataInputPlaceholderColor}
+								borderColor={customStyleProps.userDataInputBorderColor}
+								size={InputSizes.Small}
+								keyboardType={TKeyboardKeys.emailAddress}
+								blurOnSubmit={true}
+								returnKeyType={TRKeyboardKeys.done}
+								onChangeText={(value: string) => setFieldValue('email', value)}
+								focusUpdateHandler={(value) => !value && handleBlur('email')}
+							/>
+							{errors.email && <Text style={style.errorText}>{errors.email}</Text>}
+						</View>
+						<View style={style.miningContainer}>
+							<SettingCheckbox
+								title={getText('settings.screen.mining.title')}
+								description={getText('settings.screen.mining.description')}
+								value={miningEnabledValue}
+								onValueUpdated={() => setFieldValue('miningEnabled', !miningEnabledValue, false)}
+							/>
+						</View>
+					</KeyboardAwareScrollView>
+					{isValid && (
+						<View style={style.bottomContainer}>
+							<TouchableOpacity style={style.saveButton} onPress={handleSubmit}>
+								<Text style={style.saveButtonText}>{getText('settings.screen.save.button')}</Text>
+								<Icon name={'check'} style={style.checkIcon} />
+							</TouchableOpacity>
+						</View>
+					)}
+				</View>
+			)}
+		/>
 	</WithInlineLoader>
 );
