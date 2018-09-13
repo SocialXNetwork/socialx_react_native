37922085e83e1b32ab02c4bef9a45e36c77e3bd5
ionutmovila <ionut.movila@sensidev.com>
2018-09-13 12:41:46 +0300

    Installed native lib. react-native-device-info

--- packages/RNSocialX/android/app/src/main/java/com/rnsocialx/MainApplication.java
+++ packages/RNSocialX/android/app/src/main/java/com/rnsocialx/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnsocialx;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.react.rnspinkit.RNSpinkitPackage;
 import iyegoroff.RNTextGradient.RNTextGradientPackage;
 import com.cmcewen.blurview.BlurViewPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNSpinkitPackage(),
             new RNTextGradientPackage(),
             new BlurViewPackage(),
