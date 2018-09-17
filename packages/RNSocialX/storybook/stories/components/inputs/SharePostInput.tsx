6459df9b0aae60c2a346ac798cc19ac4a1bec7a3
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-17 12:27:46 +0300

    Add SettingCheckbox and SharePostInput to storybook

---
+++ packages/RNSocialX/storybook/stories/components/inputs/SharePostInput.tsx
@@ -0,0 +1,29 @@
+import {storiesOf} from '@storybook/react-native';
+import * as React from 'react';
+import {Alert} from 'react-native';
+
+import {SharePostInput} from '../../../../src/components/';
+import CenterView from '../../../helpers/CenterView';
+
+const image = 'https://www.w3schools.com/w3css/img_lights.jpg';
+
+class SharePostInputStory extends React.Component {
+	public state = {
+		value: '',
+	};
+
+	public render() {
+		return (
+			<SharePostInput
+				avatarSource={image}
+				placeholder={'Type something'}
+				text={this.state.value}
+				onTextUpdate={(value) => this.setState({value})}
+			/>
+		);
+	}
+}
+
+storiesOf('Components/inputs', module)
+	.addDecorator((getStory: any) => <CenterView>{getStory()}</CenterView>)
+	.add('SharePostInput', () => <SharePostInputStory />);
