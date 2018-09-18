7d22984aeb5ca14046c9e09d3e67eee18f3d2dd0
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-18 10:21:45 +0300

    Split stories into components and screens so it's easier to test

--- packages/RNSocialX/storybook/stories/components/Foo.tsx
+++
@@ -1,6 +0,0 @@
-import {storiesOf} from '@storybook/react-native';
-import React from 'react';
-
-import {Foo} from '../../../src/components';
-
-storiesOf('Components', module).add('Foo', () => <Foo message={'Hey there, I am Foo!'} style={{}} />);
---
+++ packages/RNSocialX/storybook/stories/components/index.ts
@@ -0,0 +1,60 @@
+import './activityCards/ActivityFriendRequestCard';
+import './activityCards/ActivityGenericCard';
+import './activityCards/ActivityRecentCommentCard';
+import './activityCards/ActivitySuperLikedCard';
+
+import './avatar/AvatarImage';
+import './avatar/AvatarName';
+import './avatar/AvatarPicker';
+
+import './displayers/AddFriendsList';
+import './displayers/CheckboxButtonWithIcon';
+import './displayers/CommentCard';
+import './displayers/DotsMenuButton';
+import './displayers/HeartAnimation';
+import './displayers/MediaHorizontalScroller';
+import './displayers/MediaObjectViewer';
+import './displayers/NavigationItems';
+import './displayers/PeopleSearchResultEntry';
+import './displayers/ProfilePhotoGrid';
+import './displayers/ProfileTopContainer';
+import './displayers/RichText';
+import './displayers/TextGradient';
+import './displayers/WallPostCard/BestComments';
+import './displayers/WallPostCard/CommentInput';
+import './displayers/WallPostCard/PostText';
+import './displayers/WallPostCard/RecentLikes';
+import './displayers/WallPostCard/TaggedFriends';
+import './displayers/WallPostCard/UserDetails';
+import './displayers/WallPostCard/ViewAllComments';
+import './displayers/WallPostCard/WallPostActions';
+import './displayers/WallPostCard/WallPostCard';
+import './displayers/WallPostCard/WallPostMedia';
+
+import './hocs/WithInlineLoader';
+
+import './inputs/CommentTextInput';
+import './inputs/PrimaryTextInput';
+import './inputs/SettingCheckbox';
+import './inputs/SharePostInput';
+
+import './interaction/CloseButton';
+import './interaction/GradientButton';
+import './interaction/IconButton';
+import './interaction/LikeAnimatingButton';
+import './interaction/PrimaryButton';
+import './interaction/SuggestionsCarousel';
+import './interaction/ToggleIconButton';
+import './interaction/TouchableWithDoublePress';
+import './interaction/VideoPlayer';
+
+import './modals/ActivityIndicatorModal';
+import './modals/ConfirmationModal';
+import './modals/InputSMSCodeModal';
+import './modals/MediaInfoModal';
+import './modals/OfflineOverlayModal';
+import './modals/ReportProblemModal';
+import './modals/TagFriendsModal';
+
+import './navigation/HeaderLogo';
+import './navigation/ScreenHeaderButton';
