e19db632847cc166a8c8b86ab187670156901405
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-18 13:32:36 +0300

    Add UserFeedScreen to storybook

--- packages/RNSocialX/storybook/stories/screens/index.ts
+++ packages/RNSocialX/storybook/stories/screens/index.ts
@@ -4,6 +4,7 @@ import './preAuth/LoginScreen';
 import './preAuth/LaunchScreen';
 import './intro/IntroScreen';
 import './preAuth/ForgotPasswordScreen';
+import './mainTabNav/UserFeedScreen';
 import './mainStack/UserProfileScreen';
 import './primaryNav/MaintenanceScreen';
 import './commentsStack/CommentsScreen';
