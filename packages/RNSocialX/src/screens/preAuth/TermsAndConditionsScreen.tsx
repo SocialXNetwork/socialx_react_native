07b8a0494d99fea184ab96c11377393be2a7907a
Alex Sirbu <alex.sirbu@sensidev.com>
2018-09-20 13:17:40 +0300

    Add the Header component to TermsAndConditionsScreen

--- packages/RNSocialX/src/screens/preAuth/TermsAndConditionsScreen.tsx
+++ packages/RNSocialX/src/screens/preAuth/TermsAndConditionsScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Platform, View} from 'react-native';
+import {Platform} from 'react-native';
 
 import {
 	IWithTermsAndConditionsEnhancedActions,
@@ -16,13 +16,23 @@ type ITermsAndConditionsScreenProps = INavigationProps &
 	IWithTermsAndConditionsEnhancedActions &
 	IWithTermsAndConditionsEnhancedData;
 
-const Screen: React.SFC<ITermsAndConditionsScreenProps> = () => {
+const onGoBackHandler = (navigation: any) => {
+	navigation.goBack(null);
+};
+
+const Screen: React.SFC<ITermsAndConditionsScreenProps> = ({navigation, getText}) => {
 	const webViewLocalSource =
 		Platform.OS === OS_TYPES.IOS
 			? TermsAndConditionsHTML
 			: {uri: 'file:///android_asset/html/terms_and_conditions.html'};
 
-	return <TermsAndConditionsScreenView localSource={webViewLocalSource} />;
+	return (
+		<TermsAndConditionsScreenView
+			localSource={webViewLocalSource}
+			onGoBack={() => onGoBackHandler(navigation)}
+			getText={getText}
+		/>
+	);
 };
 
 export const TermsAndConditionsScreen = ({navigation, navigationOptions}: INavigationProps) => (
@@ -32,9 +42,3 @@ export const TermsAndConditionsScreen = ({navigation, navigationOptions}: INavig
 		)}
 	</WithTermsAndConditions>
 );
-
-// @ts-ignore
-TermsAndConditionsScreen.navigationOptions = ({options}: INavigationProps) => ({
-	title: options.getText('terms.and.conditions.screen.title'),
-	headerRight: <View />,
-});
--- packages/RNSocialX/src/screens/preAuth/TermsAndConditionsScreen.view.tsx
+++ packages/RNSocialX/src/screens/preAuth/TermsAndConditionsScreen.view.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
 import {SafeAreaView, WebView} from 'react-native';
 
+import {Header, HeaderButton} from '../../components';
+import {ITranslatedProps} from '../../types';
 import styles from './TermsAndConditionsScreen.style';
 
-export const TermsAndConditionsScreenView: React.SFC<{localSource: any}> = ({localSource}) => (
+interface ITermsAndConditionsScreenViewProps extends ITranslatedProps {
+	localSource: any;
+	onGoBack: () => void;
+}
+
+export const TermsAndConditionsScreenView: React.SFC<ITermsAndConditionsScreenViewProps> = ({
+	localSource,
+	onGoBack,
+	getText,
+}) => (
 	<SafeAreaView style={styles.container}>
+		{
+			// @ts-ignore
+			<Header
+				title={getText('terms.and.conditions.screen.title')}
+				// @ts-ignore
+				left={<HeaderButton iconName={'ios-arrow-back'} onPress={onGoBack} />}
+			/>
+		}
 		<WebView source={localSource} />
 	</SafeAreaView>
 );
