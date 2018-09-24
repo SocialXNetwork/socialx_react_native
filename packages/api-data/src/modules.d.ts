1b3a19fa5bab88d08d6a21d4005b12a67a553a0c
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-09-25 01:35:07 +0300

    promisify callbacks on the api

--- packages/api-data/src/modules.d.ts
+++ packages/api-data/src/modules.d.ts
@@ -1,2 +1,8 @@
 declare module 'gun';
+
 declare module 'gun/gun';
+
+declare module 'es6-promisify' {
+	type ReturnType<T> = () => Promise<T>;
+	export function promisify<T>(fn: any): ReturnType<T>;
+}
