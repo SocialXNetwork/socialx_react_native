46a18675561f07a1a1721b0dee1dfd9d9ebeb9ce
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-09-25 18:56:56 +0300

    add all schemas

--- packages/api-data/src/repository/comments/schemas.ts
+++ packages/api-data/src/repository/comments/schemas.ts
@@ -1,1 +1,50 @@
-export default {};
+import yup from 'yup';
+
+const idType = yup
+	.string()
+	.trim()
+	.min(8)
+	.max(32)
+	.required();
+
+const longTextType = yup
+	.string()
+	.trim()
+	.min(1)
+	.max(4096);
+
+export const getPostComments = yup
+	.object()
+	.shape({
+		postId: idType,
+	})
+	.required();
+
+export const getPostLikes = yup
+	.object()
+	.shape({
+		commentId: idType,
+	})
+	.required();
+
+export const likeComment = yup
+	.object()
+	.shape({
+		commentId: idType,
+	})
+	.required();
+
+export const createComment = yup
+	.object()
+	.shape({
+		text: longTextType.required(),
+		postId: idType,
+	})
+	.required();
+
+export default {
+	getPostComments,
+	getPostLikes,
+	likeComment,
+	createComment,
+};
--- packages/api-data/src/repository/profiles/schemas.ts
+++ packages/api-data/src/repository/profiles/schemas.ts
@@ -1,1 +1,58 @@
-export default {};
+import yup from 'yup';
+
+const usernameOrPasswordType = yup
+	.string()
+	.trim()
+	.min(6)
+	.max(32)
+	.required();
+
+const longTextType = yup
+	.string()
+	.trim()
+	.min(1)
+	.max(4096);
+
+export const publicKeyInput = yup
+	.object()
+	.shape({
+		username: usernameOrPasswordType,
+	})
+	.required();
+
+export const getProfileByUsername = yup
+	.object()
+	.shape({
+		username: usernameOrPasswordType,
+	})
+	.required();
+
+export const createProfileInput = yup
+	.object()
+	.shape({
+		username: usernameOrPasswordType,
+		name: yup
+			.string()
+			.trim()
+			.min(4)
+			.max(128)
+			.required(),
+		email: yup
+			.string()
+			.trim()
+			.lowercase()
+			.email()
+			.required(),
+		avatar: yup
+			.string()
+			.trim()
+			.required(),
+		pub: longTextType.required(),
+	})
+	.required();
+
+export default {
+	publicKeyInput,
+	getProfileByUsername,
+	createProfileInput,
+};
