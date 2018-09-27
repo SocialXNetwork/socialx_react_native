3b1de26a8f40899eb6eaed47e1a5f19b90488b27
Jake <jake@Jakes-MacBook-Pro.local>
2018-09-27 10:39:24 +0300

    implemented the rest of accounts api surface on redux

--- packages/RNSocialX/src/store/data/profiles/Types.ts
+++ packages/RNSocialX/src/store/data/profiles/Types.ts
@@ -1,4 +1,10 @@
-import { ICreateProfileInput } from '@socialx/api-data';
+import {
+	IAcceptFriendInput,
+	IAddFriendInput,
+	ICreateProfileInput,
+	IRemoveFriendInput,
+	IUpdateProfileInput,
+} from '@socialx/api-data';
 import { Action } from 'redux';
 import { DeepReadonly } from 'utility-types';
 
@@ -21,6 +27,10 @@ export const enum ActionTypes {
 	GET_PROFILE_BY_USERNAME = 'app/data/profiles/GET_PROFILE_BY_USERNAME',
 	GET_CURRENT_PROFILE = 'app/data/profiles/GET_CURRENT_PROFILE',
 	GET_PUBLIC_KEY_BY_USERNAME = 'app/data/profiles/GET_PUBLIC_KEY_BY_USERNAME',
+	UPDATE_PROFILE = 'app/data/profiles/UPDATE_PROFILE',
+	ADD_FRIEND = 'app/data/profiles/ADD_FRIEND',
+	REMOVE_FRIEND = 'app/data/profiles/REMOVE_FRIEND',
+	ACCEPT_FRIEND = 'app/data/profiles/ACCEPT_FRIEND',
 }
 
 export interface ICreateProfileAction extends Action {
@@ -42,8 +52,32 @@ export interface IGetPublicKeyByUsernameAction extends Action {
 	payload: IUsernameArgument;
 }
 
+export interface IUpdateProfileAction extends Action {
+	type: ActionTypes.UPDATE_PROFILE;
+	payload: IUpdateProfileInput;
+}
+
+export interface IAddFriendAction extends Action {
+	type: ActionTypes.ADD_FRIEND;
+	payload: IAddFriendInput;
+}
+
+export interface IRemoveFriendAction extends Action {
+	type: ActionTypes.REMOVE_FRIEND;
+	payload: IRemoveFriendInput;
+}
+
+export interface IAcceptFriendAction extends Action {
+	type: ActionTypes.ACCEPT_FRIEND;
+	payload: IAcceptFriendInput;
+}
+
 export type IAction =
 	| ICreateProfileAction
 	| IGetProfileByUsernameAction
 	| IGetCurrentProfileAction
-	| IGetPublicKeyByUsernameAction;
+	| IGetPublicKeyByUsernameAction
+	| IUpdateProfileAction
+	| IAddFriendAction
+	| IRemoveFriendAction
+	| IAcceptFriendAction;
