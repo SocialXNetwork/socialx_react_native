bc8d69b3d7d8ed728d5736d20611f6027e775b28
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-09-30 01:26:54 +0300

    guard against gun mutations

--- packages/api-data/src/utils/helpers.ts
+++ packages/api-data/src/utils/helpers.ts
@@ -40,5 +40,11 @@ export const resolveCallback = (resolve: any, reject: any) => (
 	if (e) {
 		reject(e);
 	}
-	resolve(r);
+	// We are doing this ugly thing here because objects in javascript are passed as
+	// deep references it turns out gun internally mutates references everywhere
+	// to leverage performance benefits, but this breaks redux and react in general
+	// which expects state data to be immutable. Finally, we are doing it here
+	// because this looks like the only place where all getters return their data
+	// through. Not architecturally the best place, though.
+	resolve(JSON.parse(JSON.stringify(r)));
 };
