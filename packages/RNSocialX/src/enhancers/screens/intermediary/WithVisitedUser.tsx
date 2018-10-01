b2626035c803f49bfebc4c4a433817661c5c4d90
Alex Sirbu <alex.sirbu@sensidev.com>
2018-10-01 14:27:25 +0300

    Create WithVisitedUser enhancer

---
+++ packages/RNSocialX/src/enhancers/screens/intermediary/WithVisitedUser.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+
+import { SCREENS } from '../../../environment/consts';
+import { IVisitedUser, SearchResultKind } from '../../../types';
+
+import { WithNavigationParams } from '../../connectors/app/WithNavigationParams';
+import { WithAccounts } from '../../connectors/data/WithAccounts';
+import { WithProfiles } from '../../connectors/data/WithProfiles';
+
+interface IWithVisitedUserProps {
+	children({
+		visitedUser,
+	}: {
+		visitedUser: IVisitedUser | undefined;
+	}): JSX.Element;
+}
+
+interface IWithVisitedUserState {}
+
+export class WithVisitedUser extends React.Component<
+	IWithVisitedUserProps,
+	IWithVisitedUserState
+> {
+	render() {
+		return (
+			<WithNavigationParams>
+				{(navigationProps) => (
+					<WithAccounts>
+						{({ accounts }) => (
+							<WithProfiles>
+								{({ profiles }) => {
+									const { navigationParams } = navigationProps;
+
+									const params = navigationParams[SCREENS.UserProfile];
+
+									const foundAccount = accounts.find(
+										(account) => account.alias === params.userId,
+									);
+									const foundProfile = profiles.find(
+										(profile) => profile.pub === foundAccount!.pub,
+									);
+
+									let visitedUser;
+									if (foundProfile) {
+										visitedUser = {
+											userId: foundAccount!.alias,
+											fullName: foundProfile.fullName,
+											userName: foundAccount!.alias,
+											avatarURL: foundProfile.avatar,
+											aboutMeText: foundProfile.aboutMeText,
+											numberOfLikes: 0,
+											numberOfPhotos: 0,
+											numberOfFriends: foundProfile.friends.length,
+											numberOfComments: 0,
+											mediaObjects: [],
+											recentPosts: [],
+											relationship: SearchResultKind.NotFriend,
+										};
+									}
+
+									return this.props.children({
+										visitedUser,
+									});
+								}}
+							</WithProfiles>
+						)}
+					</WithAccounts>
+				)}
+			</WithNavigationParams>
+		);
+	}
+}
