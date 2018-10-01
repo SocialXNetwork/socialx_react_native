69ba72bd4dbfcbc05391ae0512dbf8c9a2ae5a56
Alex Sirbu <alex.sirbu@sensidev.com>
2018-10-02 00:19:47 +0300

    Inject screen enhancers with transformed data

--- packages/RNSocialX/src/enhancers/screens/intermediary/WithVisitedUserContent.tsx
+++ packages/RNSocialX/src/enhancers/screens/intermediary/WithVisitedUserContent.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 
-import {
-	IMediaProps,
-	IVisitedUser,
-	MediaTypeImage,
-	MediaTypeVideo,
-} from '../../../types';
+import { IVisitedUser } from '../../../types';
+import { extractMediaFromPosts, mapPostsForUI } from '../../helpers';
 
 import { ActionTypes } from '../../../store/data/posts/Types';
 import { WithConfig } from '../../connectors/app/WithConfig';
@@ -30,103 +26,29 @@ export class WithVisitedUserContent extends React.Component<
 	render() {
 		return (
 			<WithConfig>
-				{(configProps) => (
+				{({ appConfig }) => (
 					<WithPosts>
-						{(postsProps) => (
+						{({ posts }) => (
 							<WithVisitedUser>
 								{({ visitedUser }) => (
 									<WithActivities>
-										{(activitiesProps) => {
+										{({ activities }) => {
 											const user = visitedUser;
-											const { activities } = activitiesProps;
-
-											const posts = postsProps.posts.filter(
-												(post) => post.owner.alias === user!.userId,
-											);
-
-											const recentPosts = posts
-												.sort((x: any, y: any) => y.timestamp - x.timestamp)
-												.slice(0, 5)
-												.map((post) => {
-													const foundLike = post.likes.find(
-														(like) => like.owner.alias === user!.userId,
-													);
-
-													return {
-														id: post.postId,
-														postText: post.postText,
-														location: post.location,
-														taggedFriends: post.taggedFriends,
-														timestamp: new Date(post.timestamp * 1000),
-														owner: {
-															userId: post.owner.alias,
-															fullName: user!.fullName,
-															avatarURL: user!.avatarURL,
-														},
-														governanceVersion: post.governanceVersion,
-														// TODO: add this later when data is available
-														numberOfSuperLikes: 0,
-														numberOfComments: post.comments.length,
-														// TODO: add this later when data is available
-														numberOfWalletCoins: 0,
-														likedByMe: !!foundLike,
-														canDelete: true,
-														media: post.media!.map((media) => ({
-															url:
-																configProps.appConfig.ipfsConfig.ipfs_URL +
-																media.hash,
-															hash: media.hash,
-															type:
-																media.type.name === 'Photo'
-																	? MediaTypeImage
-																	: MediaTypeVideo,
-															extension: media.extension,
-															size: media.size,
-															numberOfLikes: post.likes.length,
-															numberOfComments: post.comments.length,
-														})),
-														likes: post.likes.map((like) => {
-															return {
-																userId: like.owner.alias,
-																userName: like.owner.alias,
-															};
-														}),
-														bestComments: post.comments
-															.slice(0, 2)
-															.map((comment) => {
-																return {
-																	id: String(comment.timestamp),
-																	text: comment.text,
-
-																	likes: Object.keys(comment.likes).map(
-																		(key) => {
-																			const like = comment.likes[key];
-																			return {
-																				userId: like.owner.alias,
-																				userName: like.owner.alias,
-																			};
-																		},
-																	),
-																	owner: {
-																		userId: comment.owner.alias,
-																		userName: comment.owner.alias,
-																	},
-																};
-															}),
-														listLoading:
-															activities.filter(
-																(activity) =>
-																	activity.type ===
-																	ActionTypes.GET_POSTS_BY_USER,
-															).length > 0,
-														suggested: undefined,
-														noInput: true,
-														contentOffensive: false,
-														marginBottom: 0,
-													};
-												});
 
 											if (user) {
+												const userPosts = posts.filter(
+													(post) => post.owner.alias === user.userId,
+												);
+
+												const recentPosts = mapPostsForUI(
+													userPosts,
+													5,
+													user,
+													activities,
+													ActionTypes.GET_POSTS_BY_USER,
+													appConfig,
+												);
+
 												user.numberOfLikes = posts.reduce(
 													(acc, post) => acc + post.likes.length,
 													0,
@@ -143,27 +65,9 @@ export class WithVisitedUserContent extends React.Component<
 													0,
 												);
 
-												user.mediaObjects = posts.reduce(
-													(acc: IMediaProps[], post) =>
-														post.media
-															? acc.concat(
-																	post.media.map((media) => ({
-																		url:
-																			configProps.appConfig.ipfsConfig
-																				.ipfs_URL + media.hash,
-																		hash: media.hash,
-																		type:
-																			media.type.name === 'Photo'
-																				? MediaTypeImage
-																				: MediaTypeVideo,
-																		extension: media.extension,
-																		size: media.size,
-																		numberOfLikes: post.likes.length,
-																		numberOfComments: post.comments.length,
-																	})),
-															  ) // tslint:disable-line indent (tslint bug!!!)
-															: [...acc],
-													[],
+												user.mediaObjects = extractMediaFromPosts(
+													posts,
+													appConfig,
 												);
 
 												user.recentPosts = recentPosts;
