02bd5861a52dd0ec6536186edb31e943fb048bd3
Alexandre Sobolevski <alex.sobolevski@instant.co>
2018-10-03 12:01:37 -0400

    Fixes posts.getPostsByPath method and its relevant getter
    - adds moment-timezone to format current test date
    - post creation tests

--- packages/api-data/src/__tests__/posts.test.ts
+++ packages/api-data/src/__tests__/posts.test.ts
@@ -1,3 +1,4 @@
+import moment from 'moment-timezone';
 import { dataApiFactory } from '../__testHelpers/mockApi';
 import records from '../__testHelpers/records';
 
@@ -58,52 +59,70 @@ describe('posts api', () => {
 			}
 			const post = getPost();
 			await mockApi.posts.createPost(post);
+			const createdPost = await mockApi.posts.getPostByPath({
+				postPath: `${moment().format('YYYY/M/D')}/public`,
+			});
+			// TODO: do a more strict comparison
+			expect(createdPost).toBeTruthy();
 		} catch (e) {
 			expect(e).toBeUndefined();
 		}
 	});
 
-	test('reject get post by path', () => {
+	test('reject get post by path', async () => {
+		let error: any;
+		try {
+			if (!mockApi) {
+				throw new Error('mockApi is not defined');
+			}
+			const post = getPost();
+			await mockApi.posts.createPost(post);
+			await mockApi.posts.getPostByPath({
+				postPath: 'abcd',
+			});
+		} catch (e) {
+			error = e;
+		}
+		expect(error).toMatch('no post found');
+	});
+
+	test('get posts by user', async () => {
 		/**/
 	});
 
-	test('get posts by user', () => {
+	test('reject get posts by user', async () => {
 		/**/
 	});
 
-	test('reject get posts by user', () => {
+	test('get public posts by date', async () => {
 		/**/
 	});
 
-	test('get public posts by date', () => {
+	test('reject get public posts by date', async () => {
 		/**/
 	});
 
-	test('reject get public posts by date', () => {
+	test('likes a post', async () => {
 		/**/
 	});
 
-	test('likes a post', () => {
+	test('reject likes a post', async () => {
 		/**/
 	});
 
-	test('reject likes a post', () => {
+	test('removes a post', async () => {
 		/**/
 	});
 
-	test('removes a post', () => {
+	test('reject removes a post', async () => {
 		/**/
 	});
 
-	test('reject removes a post', () => {
+	test('unlike a post', async () => {
 		/**/
 	});
 
-	test('unlike a post', () => {
-		/**/
-	});
-
-	test('rejects unlike a post', () => {
+	test('rejects unlike a post', async () => {
 		/**/
 	});
 });
