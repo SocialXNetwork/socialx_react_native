c838c20c7b7ed0dd7b3b59b53fe0a86de515836f
Alexandre Sobolevski <alex.sobolevski@instant.co>
2018-10-04 15:29:02 -0400

    Create specific error classes and tests

---
+++ packages/api-data/src/utils/errors/ExtensibleError.ts
@@ -0,0 +1,28 @@
+export default class ExtensibleError extends Error {
+	name: string;
+	message: string;
+	code: number | undefined;
+	details: object | undefined;
+	loggedMessage: any;
+	log: () => void;
+	constructor(message: string, additionalInformation: any = {}) {
+		super(message);
+		this.name = this.constructor.name;
+		this.message = message || 'no message provided';
+		this.details = additionalInformation.details;
+		this.code = additionalInformation.code;
+		this.loggedMessage = [
+			`An Error occured: ${message}`,
+			this.code && process.env.DEBUG ? `Code: ${this.code}` : '',
+			this.details && process.env.DEBUG
+				? `Details: ${JSON.stringify(this.details)}`
+				: '',
+		].filter((v: string) => v.length);
+
+		this.log = () => {
+			// Always log the stack
+			this.loggedMessage.push(this.stack);
+			console.log(this.loggedMessage.join('\n'));
+		};
+	}
+}
---
+++ packages/api-data/src/utils/errors/index.ts
@@ -0,0 +1,5 @@
+import ApiError from './ApiError';
+import ExtensibleError from './ExtensibleError';
+import ValidationError from './ValidationError';
+
+export { ApiError, ExtensibleError, ValidationError };
