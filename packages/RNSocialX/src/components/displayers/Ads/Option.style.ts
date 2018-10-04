359f6dc3d3c7a9ead9379a3a0a89bbf8bb7e0072
Alex Sirbu <alex.sirbu@sensidev.com>
2018-10-04 20:28:10 +0300

    Create AdsManagementScreen, WIP

---
+++ packages/RNSocialX/src/components/displayers/Ads/Option.style.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Fonts, Sizes } from '../../../environment/theme';
+
+const ICON_SIZE = Sizes.smartHorizontalScale(24);
+
+const styles: any = {
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingVertical: Sizes.smartVerticalScale(10),
+		paddingHorizontal: Sizes.smartHorizontalScale(16),
+	},
+	optionContainer: {
+		flex: 8,
+		flexDirection: 'row',
+	},
+	icon: {
+		width: ICON_SIZE,
+		height: ICON_SIZE,
+		marginRight: Sizes.smartHorizontalScale(15),
+	},
+	text: {
+		...Fonts.centuryGothic,
+		fontSize: Sizes.smartHorizontalScale(16),
+	},
+	arrowContainer: {
+		flex: 1,
+		height: '100%',
+		alignItems: 'flex-end',
+	},
+	arrow: {
+		fontSize: ICON_SIZE,
+		color: Colors.pink,
+	},
+};
+
+export default StyleSheet.create(styles);
