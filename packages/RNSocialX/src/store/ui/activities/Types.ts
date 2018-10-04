5aa40506d6548e2cc37b8b4aafd6de4eb325876b
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-10-04 22:58:57 +0300

    add example for @Alexandre for redux error tracking

--- packages/RNSocialX/src/store/ui/activities/Types.ts
+++ packages/RNSocialX/src/store/ui/activities/Types.ts
@@ -1,6 +1,5 @@
 import { Action } from 'redux';
 import { DeepReadonly } from 'utility-types-fixme-todo';
-import { ISetAuthAction } from '../../app/auth/Types';
 
 export interface IActivity {
 	uuid: string;
