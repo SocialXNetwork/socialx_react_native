41320b547cb12a5655dc72c3c16f593a938065ce
Alexandre Sobolevski <alex.sobolevski@instant.co>
2018-10-05 08:24:47 -0400

    Fix error class declarations

--- packages/api-data/src/utils/errors/ApiError.ts
+++ packages/api-data/src/utils/errors/ApiError.ts
@@ -1,21 +1,15 @@
 import ExtensibleError from './ExtensibleError';
 
 export default class ApiError extends ExtensibleError {
-	initialRequestBody: object | undefined = {};
-	statusReturned: number | undefined = 0;
+	initialRequestBody?: object;
 	constructor(message: string, additionalInformation: any = {}) {
 		super(message, additionalInformation);
-		this.statusReturned = additionalInformation.statusReturned;
 		this.initialRequestBody = additionalInformation.initialRequestBody;
-		if (this.statusReturned && process.env.DEBUG) {
-			this.loggedMessage.push(
-				`Request Returned Status: ${this.statusReturned}`,
-			);
-		}
 		if (this.initialRequestBody && process.env.DEBUG) {
-			this.loggedMessage.push(
+			this.loggedMessage = [
+				...this.loggedMessage,
 				`Initial Request: ${JSON.stringify(this.initialRequestBody)}`,
-			);
+			];
 		}
 	}
 }
--- packages/api-data/src/utils/errors/ValidationError.ts
+++ packages/api-data/src/utils/errors/ValidationError.ts
@@ -1,14 +1,15 @@
 import ExtensibleError from './ExtensibleError';
 
 export default class ValidationError extends ExtensibleError {
-	validationInput: any | undefined;
+	validationInput?: object;
 	constructor(message: string, additionalInformation: any = {}) {
 		super(message, additionalInformation);
 		this.validationInput = additionalInformation.validationInput;
 		if (this.validationInput && process.env.DEBUG) {
-			this.loggedMessage.push(
+			this.loggedMessage = [
+				...this.loggedMessage,
 				`Validation Input : ${JSON.stringify(this.validationInput)}`,
-			);
+			];
 		}
 	}
 }
