8132c0115cb56970653cfc32e35fd7d182411ba0
ionutmovila <ionut.movila@sensidev.com>
2018-10-08 11:23:13 +0300

    Small fixes

--- packages/RNSocialX/index.js
+++ packages/RNSocialX/index.js
@@ -7,3 +7,5 @@ import { App } from './src/app';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+console.disableYellowBox = true; // we should get this warning in debugger and not over the screen in the app.
