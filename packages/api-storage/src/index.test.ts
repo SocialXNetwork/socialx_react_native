04a8ea98ff973b2521a9b36d1085b3b7504d3598
Alexandre Sobolevski <alex.sobolevski@instant.co>
2018-10-08 19:06:51 -0400

    Get ipsf tests compiling

--- packages/api-storage/src/index.test.ts
+++ packages/api-storage/src/index.test.ts
@@ -1,3 +1,21 @@
-test('index', () => {
-	expect(index()).toBe('index');
+import { storageApiFactory } from './index';
+
+describe('ipfs', () => {
+	test('instantiates correctly', async () => {
+		try {
+			const storageApi = storageApiFactory();
+			expect(storageApi).toBeTruthy();
+		} catch (e) {
+			throw new Error(e);
+		}
+	});
+
+	// test('adds file', async () => {
+	// 	const storageApi = storageApiFactory();
+	// 	await storageApi.uploadFile('./test.txt');
+	// });
+	//
+	// test('gets file info');
+	//
+	// test('cancel upload');
 });
