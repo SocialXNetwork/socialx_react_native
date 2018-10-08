e9168306e28c207f952317f402179e72fb99419b
Alexandre Sobolevski <alex.sobolevski@instant.co>
2018-10-08 13:49:50 -0400

    Adds development flag to choose server host, updates README

--- packages/database/src/server.ts
+++ packages/database/src/server.ts
@@ -6,7 +6,11 @@ import 'gun/nts';
 
 import path from 'path';
 
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST =
+	process.env.NODE_ENV === 'development'
+		? '127.0.0.1'
+		: process.env.HOST || '0.0.0.0';
+
 const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 8765;
 
 const app: Application = express();
@@ -17,4 +21,4 @@ const server: Server = app.listen(PORT, HOST);
 
 Gun({ file: path.resolve(__dirname, '../data'), web: server });
 
-console.log(`Gun server listening on http://localhost:${PORT}/gun`);
+console.log(`Gun server listening on http://${HOST}:${PORT}/gun`);
