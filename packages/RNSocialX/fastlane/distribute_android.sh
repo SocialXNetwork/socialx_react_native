22b1ce20cfb5c2df13ae1fd27b7ef1e4d28262aa
ionutmovila <ionut.movila@sensidev.com>
2018-10-11 17:02:01 +0300

    Testing android distribution with Fastlane + fixes

--- packages/RNSocialX/fastlane/distribute_android.sh
+++ packages/RNSocialX/fastlane/distribute_android.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ANDROID_VERSION_STRING="0.0.5"
-export ANDROID_VERSION_CODE=5
+export ANDROID_VERSION_STRING="0.0.6"
+export ANDROID_VERSION_CODE=6
 
 bundle exec fastlane android release version_string:${ANDROID_VERSION_STRING} version_code:${ANDROID_VERSION_CODE}
