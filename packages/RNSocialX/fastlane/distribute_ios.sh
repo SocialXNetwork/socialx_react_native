4e58aa4565115c702f5833f37965b60452ae28cd
ionutmovila <ionut.movila@sensidev.com>
2018-10-15 17:47:56 +0300

    Misc, minor fixes for iOS Fastlane

--- packages/RNSocialX/fastlane/distribute_ios.sh
+++ packages/RNSocialX/fastlane/distribute_ios.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-export IOS_VERSION_STRING="0.0.6"
-export IOS_VERSION_CODE=6
+export IOS_VERSION_STRING="0.0.8"
+export IOS_VERSION_CODE=8
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 bundle exec fastlane ios release version_string:${IOS_VERSION_STRING} version_code:${IOS_VERSION_CODE}
