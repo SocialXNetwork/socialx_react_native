175f5cd825b5e0c9bf07d92660a46a5f2fcd7902
ionutmovila <ionut.movila@sensidev.com>
2018-10-16 12:23:16 +0300

    Misc: fixed persisted i18n dictionaries => should not be persisted!

--- packages/RNSocialX/src/app/Store.tsx
+++ packages/RNSocialX/src/app/Store.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 // tslint:disable-next-line:no-submodule-imports
