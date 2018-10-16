0209fae8264f1597b96c9f4ac423458def901e26
ionutmovila <ionut.movila@sensidev.com>
2018-10-16 12:23:16 +0300

    Misc: fixed persisted i18n dictionaries => should not be persisted!

--- packages/RNSocialX/src/store/app/i18n/reducer.ts
+++ packages/RNSocialX/src/store/app/i18n/reducer.ts
@@ -4,6 +4,13 @@ import { ActionTypes, IAction, IState } from './Types';
 
 export default (state: IState = initialState, action: IAction): IState => {
 	switch (action.type) {
+		case ActionTypes.REHYDRATE: {
+			const { dictionary, ...others } = action.payload.app.i18n;
+			return {
+				...state,
+				...others,
+			};
+		}
 		case ActionTypes.SET_LOCALE: {
 			return {
 				...state,
