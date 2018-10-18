5873e75f8ce6ce457dd8931bf661475ec3636bbd
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-10-18 12:42:40 -0400

    Adds the rest of the handles

--- packages/api-data/src/extensions/handles.ts
+++ packages/api-data/src/extensions/handles.ts
@@ -2,6 +2,44 @@
 import * as Gun from 'gun/gun';
 import { TABLES, TABLE_ENUMS } from '../types';
 
+// Accounts
+Gun.chain.currentAccountRecover = function()  {
+	return this.user().get(TABLE_ENUMS.RECOVER);
+};
+
+Gun.chain.accountByPub = function( pub: string)  {
+	return this.user(pub);
+};
+
+// Comments
+Gun.chain.commentsByPostPath = function( postPath: string) {
+	return this.get(TABLES.POSTS).path(postPath).get(TABLE_ENUMS.COMMENTS);
+}
+
+Gun.chain.commentMetaById = function( commentId: string)  {
+	return this.get(TABLES.COMMENT_META_BY_ID).get(commentId);
+};
+
+Gun.chain.likesByCommentPath = function( commentPath: string)  {
+	return this.path(commentPath).get(TABLE_ENUMS.LIKES);
+};
+
+// Notifications
+Gun.chain.notificationsByUsername = function(
+	username: string,
+)  {
+	return this.get(TABLES.NOTIFICATIONS).get(username);
+};
+
+Gun.chain.notifications = function()  {
+	return this.get(TABLES.NOTIFICATIONS).get(this.user().is.alias);
+};
+
+Gun.chain.notificationById = function( notificationId: string)  {
+	return this.get(TABLES.NOTIFICATIONS).get(notificationId);
+};
+
+// Profiles
 Gun.chain.getUserProfile = function(
   username: string,
 ) {
@@ -55,4 +93,44 @@ Gun.chain.userProfileFriendship = function(
 		.get(username)
 		.get(TABLE_ENUMS.FRIENDS)
 		.get(friendshipId);
+};
+
+// Posts
+Gun.chain.postMetaById = function (postId: string) {
+	return this.get(TABLES.POST_META_BY_ID).get(postId);
+};
+
+Gun.chain.postMetasByUsername = function (username: string) {
+	return this.get(TABLES.POST_METAS_BY_USER).get(username);
+};
+
+Gun.chain.postMetasByCurrentUser = function () {
+	return this.get(TABLES.POST_METAS_BY_USER).get(this.user().is.alias);
+};
+
+Gun.chain.postMetasByPostIdOfCurrentAccount = function (
+	postId: string,
+) {
+	return this
+		.get(TABLES.POST_METAS_BY_USER)
+		.get(this.user().is.alias)
+		.get(postId);
+};
+
+Gun.chain.postByPath = function (postPath: string) {
+	return this.get(TABLES.POSTS).path(postPath);
+};
+
+Gun.chain.postsByDate = function (datePath: string) {
+	return this.path(`${TABLES.POSTS}.${datePath}.${TABLE_ENUMS.PUBLIC}`);
+};
+
+Gun.chain.likesByPostPath = function (postPath: string) {
+	return this.get(TABLES.POSTS).path(`${postPath}.${TABLE_ENUMS.LIKES}`);
+};
+
+Gun.chain.postLikesByCurrentUser = function (postPath: string) {
+	return this.path(
+		`${TABLES.POSTS}.${postPath}.${TABLE_ENUMS.LIKES}.${this.user().is.alias}`,
+	);
 };
