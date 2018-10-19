cc24df8735d2fac8cc6d63b976db41f86d179135
Jake <imedevil@yahoo.com>
2018-10-19 14:21:20 +0300

    added logout chain functionality

---
+++ packages/api-data/src/extensions/logout.ts
@@ -0,0 +1,20 @@
+import * as Gun from 'gun/gun';
+import 'gun/sea';
+import { IContext } from '../types';
+
+// tslint:disable
+Gun.chain.logout = function(context: IContext) {
+    const {account, gun} = context;
+    if (!account.is) {
+        return;
+    }
+
+    const rootUser = this.back(-1)._.user;
+    if (rootUser) {
+        delete rootUser.is;
+        delete rootUser._.is;
+        delete rootUser._.sea;
+    };
+    // @ts-ignore
+    account = new Gun({peers: context.config.peers}).user;
+};
