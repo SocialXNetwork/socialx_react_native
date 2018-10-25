2433aeb5aeb56739f4f525e1054a2204337cbb1c
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-10-25 07:53:41 -0400

    Adds notifications types table

--- packages/api-data/src/repository/notifications/handles.ts
+++ packages/api-data/src/repository/notifications/handles.ts
@@ -1,4 +1,5 @@
 import { IContext, TABLES } from '../../types';
+import { NOTIFICATION_TABLE_NAMES } from './types';
 
 export const notificationsByUsername = (
 	context: IContext,
@@ -15,4 +16,22 @@ export const notifications = (context: IContext) => {
 
 export const notificationById = (context: IContext, notificationId: string) => {
 	return notifications(context).path(notificationId);
+};
+
+export const friendRequests = (context: IContext) => {
+	const { gun, account } = context;
+	return gun.path(
+		`${TABLES.NOTIFICATIONS}.${NOTIFICATION_TABLE_NAMES.FRIEND_REQUEST}.${
+			account.is.alias
+		}`,
+	);
+};
+
+export const friendRequestsResponses = (context: IContext) => {
+	const { gun, account } = context;
+	return gun.path(
+		`${TABLES.NOTIFICATIONS}.${
+			NOTIFICATION_TABLE_NAMES.FRIEND_REQUEST_RESPONSE
+		}.${account.is.alias}`,
+	);
 };
