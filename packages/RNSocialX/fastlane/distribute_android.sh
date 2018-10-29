e6ca8ee7cc96a264c108b4856e2ac1b1592c7639
Jake <imedevil@yahoo.com>
2018-10-29 11:54:50 +0200

    android version bump @0.1.1, fixed a problem while distributing with component naming

--- packages/RNSocialX/fastlane/distribute_android.sh
+++ packages/RNSocialX/fastlane/distribute_android.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export ANDROID_VERSION_STRING="0.0.6"
-export ANDROID_VERSION_CODE=6
+export ANDROID_VERSION_STRING="0.1.1"
+export ANDROID_VERSION_CODE=11
+
+export FASTLANE_PASSWORD=""
 
 bundle exec fastlane android release version_string:${ANDROID_VERSION_STRING} version_code:${ANDROID_VERSION_CODE}
