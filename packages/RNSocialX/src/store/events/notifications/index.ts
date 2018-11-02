d2b0cb29c9045937bfd10d529792d57e16ea6334
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-11-02 16:51:03 -0400

    #5 Creates an events store to hold notifications

---
+++ packages/RNSocialX/src/store/events/notifications/index.ts
@@ -0,0 +1,10 @@
+export { default as reducer } from './reducer';
+export {
+	IState,
+	IAction,
+	IGetUserEventsAction,
+	ISyncGetUserEventsAction,
+	IGetUserEventsInput,
+} from './Types';
+export { INotificationsReturnData } from '@socialx/api-data';
+export { getUserEvents } from './actions';
