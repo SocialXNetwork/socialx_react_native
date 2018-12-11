dacfa1c36ad27559405f82dea42a682dc16daba8
Alexandru Croitoru <alex.croitoru@sensidev.com>
2018-12-11 13:28:27 +0200

    keyboardDidShow working

--- packages/RNSocialX/src/screens/mainTabNav/UserFeedScreen/UserFeedScreen.tsx
+++ packages/RNSocialX/src/screens/mainTabNav/UserFeedScreen/UserFeedScreen.tsx
@@ -29,17 +29,17 @@ type IUserFeedScreenProps = INavigationProps &
 export class Screen extends React.Component<IUserFeedScreenProps> {
 	private listRef: React.RefObject<FlatList<string>> = React.createRef();
 	private keyboardWillShowListener: any;
+	private keyboardDidShowListener: any;
 	private keyboardHeight: number = 0;
 
 	public componentDidMount() {
 		this.keyboardWillShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
-		if (Platform.OS === OS_TYPES.Android) {
-			AndroidKeyboardAdjust.setAdjustPan();
-		}
+		this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow);
 	}
 
 	public componentWillUnmount() {
 		this.keyboardWillShowListener.remove();
+		this.keyboardDidShowListener.remove();
 	}
 
 	public render() {
@@ -79,7 +79,13 @@ export class Screen extends React.Component<IUserFeedScreenProps> {
 		if (this.keyboardHeight === 0) {
 			this.setState({ keyboardHeight: e.endCoordinates.height });
 			this.keyboardHeight = e.endCoordinates.height;
+			console.log('keyboardWillShow');
+			console.log(this.keyboardHeight);
 		}
+	};
+
+	private keyboardDidShow = (e: any) => {
+		console.log('keyboard shown');
 	};
 
 	private onLoadMorePostsHandler = async () => {
@@ -121,6 +127,7 @@ export class Screen extends React.Component<IUserFeedScreenProps> {
 	};
 
 	private computeScrollOffset = (y: number, height: number, first: boolean) => {
+		console.log('computeScrollOffset');
 		console.log({ y, height });
 		if (first && y * 3 > height) {
 			return 0;
@@ -129,7 +136,7 @@ export class Screen extends React.Component<IUserFeedScreenProps> {
 		const offsetPredictor = -0.99969 * height + 346.07015;
 		const screenPredictor = 0.78309 * SCREEN_HEIGHT - 526.08524;
 		const keyboardHeightDifference = this.keyboardHeight - BASELINE_KEYBOARD_HEIGHT;
-		console.log(y - offsetPredictor - (screenPredictor - Math.abs(keyboardHeightDifference)));
+		// console.log(y - offsetPredictor - (screenPredictor - Math.abs(keyboardHeightDifference)));
 		if (SCREEN_HEIGHT !== BASELINE_SCREEN_HEIGHT) {
 			return y - offsetPredictor - (screenPredictor - Math.abs(keyboardHeightDifference));
 		} else {
