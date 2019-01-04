797904e600442758e1f12aeb61d7d010c212e05e
Alexandru Croitoru <alex.croitoru@sensidev.com>
2019-01-04 14:35:47 +0200

    Added History to mocks for Rewards Screen

--- packages/RNSocialX/src/mocks.ts
+++ packages/RNSocialX/src/mocks.ts
@@ -11,7 +11,7 @@ import {
 	TransactionType,
 } from './types';
 
-import { CoinSymbol } from './environment/consts';
+import { CoinSymbol, IRewardsHistoryData } from './environment/consts';
 
 import dictionary from './store/app/i18n/data/dictionary.en';
 import { getText } from './store/app/i18n/helpers';
@@ -400,6 +400,227 @@ export const trendingContentItems: ITrendingContentItem[] = [
 			{ type: 'Image', url: CONTENT_IMAGE_URL, postId: '1875', middle: true },
 			{ type: 'Image', url: CONTENT_IMAGE_URL, postId: '1875' },
 		],
+	},
+];
+
+export const dailyHistory: IRewardsHistoryData[] = [
+	{
+		date: '1st',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: '2nd',
+		amount: 12,
+		currency: 'SOCX',
+	},
+	{
+		date: '3rd',
+		amount: 43,
+		currency: 'SOCX',
+	},
+	{
+		date: '4th',
+		amount: 17,
+		currency: 'SOCX',
+	},
+	{
+		date: '5th',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: '6th',
+		amount: 67,
+		currency: 'SOCX',
+	},
+	{
+		date: '7th',
+		amount: 50,
+		currency: 'SOCX',
+	},
+	{
+		date: '8th',
+		amount: 32,
+		currency: 'SOCX',
+	},
+	{
+		date: '9th',
+		amount: 10,
+		currency: 'SOCX',
+	},
+	{
+		date: '10th',
+		amount: 27,
+		currency: 'SOCX',
+	},
+	{
+		date: '11st',
+		amount: 78,
+		currency: 'SOCX',
+	},
+	{
+		date: '12nd',
+		amount: 87,
+		currency: 'SOCX',
+	},
+	{
+		date: '13rd',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: '14th',
+		amount: 12,
+		currency: 'SOCX',
+	},
+	{
+		date: '15th',
+		amount: 43,
+		currency: 'SOCX',
+	},
+	{
+		date: '16th',
+		amount: 17,
+		currency: 'SOCX',
+	},
+	{
+		date: '17th',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: '18th',
+		amount: 67,
+		currency: 'SOCX',
+	},
+	{
+		date: '19th',
+		amount: 50,
+		currency: 'SOCX',
+	},
+	{
+		date: '20th',
+		amount: 32,
+		currency: 'SOCX',
+	},
+	{
+		date: '21st',
+		amount: 10,
+		currency: 'SOCX',
+	},
+	{
+		date: '22nd',
+		amount: 27,
+		currency: 'SOCX',
+	},
+	{
+		date: '23rd',
+		amount: 78,
+		currency: 'SOCX',
+	},
+	{
+		date: '24th',
+		amount: 87,
+		currency: 'SOCX',
+	},
+	{
+		date: '25th',
+		amount: 67,
+		currency: 'SOCX',
+	},
+	{
+		date: '26th',
+		amount: 50,
+		currency: 'SOCX',
+	},
+	{
+		date: '27th',
+		amount: 32,
+		currency: 'SOCX',
+	},
+	{
+		date: '28th',
+		amount: 10,
+		currency: 'SOCX',
+	},
+	{
+		date: '29th',
+		amount: 27,
+		currency: 'SOCX',
+	},
+	{
+		date: '30th',
+		amount: 78,
+		currency: 'SOCX',
+	},
+	{
+		date: '31st',
+		amount: 87,
+		currency: 'SOCX',
+	},
+];
+
+export const monthlyHistory: IRewardsHistoryData[] = [
+	{
+		date: 'Jan',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Feb',
+		amount: 12,
+		currency: 'SOCX',
+	},
+	{
+		date: 'March',
+		amount: 43,
+		currency: 'SOCX',
+	},
+	{
+		date: 'April',
+		amount: 17,
+		currency: 'SOCX',
+	},
+	{
+		date: 'May',
+		amount: 39,
+		currency: 'SOCX',
+	},
+	{
+		date: 'June',
+		amount: 67,
+		currency: 'SOCX',
+	},
+	{
+		date: 'July',
+		amount: 50,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Aug',
+		amount: 32,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Sep',
+		amount: 10,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Oct',
+		amount: 27,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Nov',
+		amount: 78,
+		currency: 'SOCX',
+	},
+	{
+		date: 'Dec',
+		amount: 87,
+		currency: 'SOCX',
 	},
 ];
 
