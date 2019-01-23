045d9dba7d393bdd5fab9aaaf6fdcdf30bafdb10
Alexandru Croitoru <alex.croitoru@sensidev.com>
2019-01-23 17:13:13 +0200

    Updated react-navigation to 3x and WIP for Android Back Button Handler

--- packages/RNSocialX/src/modules.d.ts
+++ packages/RNSocialX/src/modules.d.ts
@@ -14,6 +14,10 @@ declare module 'react-native-svg-charts';
 
 declare module 'react-native-app-intro-slider';
 
+// declare module 'react-native-image-crop-picker';
+
+// declare module 'react-native-image-resizer';
+
 declare module 'react-native-mime-types';
 
 declare module '*.json' {
