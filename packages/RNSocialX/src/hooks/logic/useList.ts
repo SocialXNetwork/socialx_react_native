d55e58c062b771b9dcaf779c0f6af050ced1933b
Jake <imedevil@yahoo.com>
2019-06-23 00:24:18 +0300

    Added list hook

---
+++ packages/RNSocialX/src/hooks/logic/useList.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export interface IListActions<T> {
+	set: (list: T[]) => void;
+	clear: () => void;
+	updateAt: (index: number, item: T) => void;
+	remove: (index: number) => void;
+	push: (item: T) => void;
+	filter: (fn: (value: T) => boolean) => void;
+	sort: (fn?: (a: T, b: T) => number) => void;
+}
+
+const useList = <T>(initialList: T[] = []): [T[], IListActions<T>] => {
+	const [list, set] = useState<T[]>(initialList);
+
+	return [
+		list,
+		{
+			set,
+			clear: () => set([]),
+			updateAt: (index, entry) =>
+				set((currentList) => [...currentList.slice(0, index), entry, ...list.slice(index + 1)]),
+			remove: (index) =>
+				set((currentList) => [...currentList.slice(0, index), ...list.slice(index + 1)]),
+			push: (entry) => set((currentList) => [...currentList, entry]),
+			filter: (fn) => set((currentList) => currentList.filter(fn)),
+			sort: (fn?) => set((currentList) => [...currentList].sort(fn)),
+		},
+	];
+};
+
+export default useList;
