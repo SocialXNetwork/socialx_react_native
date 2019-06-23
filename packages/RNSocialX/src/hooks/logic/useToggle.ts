5c64c75e369cbe3fdc2e0afa828de10ead9d6cee
Jake <imedevil@yahoo.com>
2019-06-24 00:12:09 +0300

    Added toggle hook

---
+++ packages/RNSocialX/src/hooks/logic/useToggle.ts
@@ -0,0 +1,28 @@
+import { useCallback, useState } from 'react';
+
+export type UseToggle = (
+	state: boolean,
+) => [
+	boolean, // state
+	(nextValue?: boolean) => void // toggle
+];
+
+const useToggle: UseToggle = (state) => {
+	const [value, setValue] = useState<boolean>(state);
+
+	const toggle = useCallback(
+		(nextValue?: boolean) => {
+			if (typeof nextValue !== 'undefined') {
+				setValue(!!nextValue);
+				return;
+			}
+
+			setValue((newValue) => !newValue);
+		},
+		[setValue],
+	);
+
+	return [value, toggle];
+};
+
+export default useToggle;
